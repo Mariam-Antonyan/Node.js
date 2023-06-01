3782d1e8857f267fa283f8bdbd53aae19310617a
Mariam-Antonyan <134807220+Mariam-Antonyan@users.noreply.github.com>
2023-06-02 01:06:40 +0400

    Update file-system.js

--- file-system/file-system.js
+++ file-system/file-system.js
@@ -1,76 +1,145 @@
-files.forEach((file) => {
-    console.log(`[F] ${file}`);
+const os = require('os');
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+
+const username = os.userInfo().username;
+
+console.log(`Welcome ${username}!`);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
 });
 
-files.forEach((file) => {
-    console.log(`[F] ${file}`);
+rl.on('line', (input) => {
+    const args = input.trim().split(' ');
+
+    const command = args[0];
+
+    switch (command) {
+        case '.exit':
+            exitApp();
+            break;
+        case 'os':
+            executeOsCommand(args);
+            break;
+        case 'ls':
+            listFilesAndFolders();
+            break;
+        case 'add':
+            createFile(args[1]);
+            break;
+        case 'rn':
+            renameFile(args[1], args[2]);
+            break;
+        case 'cp':
+            copyFile(args[1], args[2]);
+            break;
+        case 'mv':
+            moveFile(args[1], args[2]);
+            break;
+        case 'rm':
+            deleteFile(args[1]);
+            break;
+        default:
+            console.log('Invalid input');
+            break;
+    }
 });
+
+function exitApp() {
+    console.log(`Thank you ${username}, goodbye!`);
+    rl.close();
 }
 
-function createFile(filename) {
-    const currentDir = process.cwd();
-    const filePath = path.join(currentDir, filename);
+function executeOsCommand(args) {
+    const subCommand = args[1];
 
-    fs.writeFile(filePath, '', (err) => {
-        if (err) {
-            console.log('Error creating file:', err);
-        } else {
-            console.log('File created successfully!');
-        }
+    switch (subCommand) {
+        case '--cpus':
+            printCpusInfo();
+            break;
+        case '--homedir':
+            printHomeDir();
+            break;
+        case '--username':
+            printUsername();
+            break;
+        case '--architecture':
+            printArchitecture();
+            break;
+        case '--hostname':
+            printHostname();
+            break;
+        case '--platform':
+            printPlatform();
+            break;
+        case '--memory':
+            printMemory();
+            break;
+        default:
+            console.log('Invalid input');
+            break;
+    }
+}
+
+function printCpusInfo() {
+    const cpus = os.cpus();
+    console.log(`CPUs Info:`);
+    console.log(`Overall Amount: ${cpus.length}`);
+    cpus.forEach((cpu, index) => {
+        console.log(`CPU ${index + 1}:`);
+        console.log(`  Model: ${cpu.model}`);
+        console.log(`  Clock Rate: ${cpu.speed / 1000} GHz`);
     });
 }
 
-function renameFile(oldPath, newName) {
-    const currentDir = process.cwd();
-    const oldFilePath = path.join(currentDir, oldPath);
-    const newFilePath = path.join(currentDir, newName);
-
-    fs.rename(oldFilePath, newFilePath, (err) => {
-        if (err) {
-            console.log('Error renaming file:', err);
-        } else {
-            console.log('File renamed successfully!');
-        }
-    });
+function printHomeDir() {
+    console.log(`Home Directory: ${os.homedir()}`);
 }
 
-function copyFile(sourcePath, destinationPath) {
-    const currentDir = process.cwd();
-    const sourceFilePath = path.join(currentDir, sourcePath);
-    const destinationFilePath = path.join(currentDir, destinationPath);
-
-    fs.copyFile(sourceFilePath, destinationFilePath, (err) => {
-        if (err) {
-            console.log('Error copying file:', err);
-        } else {
-            console.log('File copied successfully!');
-        }
-    });
+function printUsername() {
+    console.log(`Username: ${username}`);
 }
 
-function moveFile(sourcePath, destinationPath) {
-    const currentDir = process.cwd();
-    const sourceFilePath = path.join(currentDir, sourcePath);
-    const destinationFilePath = path.join(currentDir, destinationPath);
-
-    fs.rename(sourceFilePath, destinationFilePath, (err) => {
-        if (err) {
-            console.log('Error moving file:', err);
-        } else {
-            console.log('File moved successfully!');
-        }
-    });
+function printArchitecture() {
+    console.log(`CPU Architecture: ${os.arch()}`);
 }
 
-function deleteFile(filePath) {
-    const currentDir = process.cwd();
-    const fileToDelete = path.join(currentDir, filePath);
+function printHostname() {
+    console.log(`Hostname: ${os.hostname()}`);
+}
 
-    fs.unlink(fileToDelete, (err) => {
-        if (err) {
-            console.log('Error deleting file:', err);
-        } else {
-            console.log('File deleted successfully!');
+function printPlatform() {
+    console.log(`Platform: ${os.platform()}`);
+}
+
+function printMemory() {
+    console.log(`Total Memory: ${os.totalmem()} bytes`);
+}
+
+function listFilesAndFolders() {
+    const currentDir = process.cwd();
+    const items = fs.readdirSync(currentDir);
+
+    console.log(`Contents of ${currentDir}:`);
+
+    const files = [];
+    const folders = [];
+
+    items.forEach((item) => {
+        const stats = fs.statSync(path.join(currentDir, item));
+
+        if (stats.isFile()) {
+            files.push(item);
+        } else if (stats.isDirectory()) {
+            folders.push(item);
         }
     });
-}
+
+    folders.sort();
+    files.sort();
+
+    folders.forEach((folder) => {
+        console.log(`[F] ${folder}`);
